9ab288c3af75005a2e244ac8f0918ba3e1afdf49
michaeljoy255 <kodiakchaos@gmail.com>
2022-06-26 21:28:31 -0400

    some basic progress, use table or inputs?

--- src/components/shared/ExerciseSetsInput.vue
+++ src/components/shared/ExerciseSetsInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { QInput, QIcon } from 'quasar'
+import { QBtn, QInput, QIcon } from 'quasar'
 import type { ExerciseSet, TrackBoolean } from '@/constants/types'
 import { useVModel } from '@vueuse/core'
 import { ref, type Ref } from 'vue'
@@ -25,6 +25,21 @@ const emits = defineEmits<{
 
 const exerciseSets = useVModel(props, 'exerciseSets', emits)
 
+function addSet() {
+  exerciseSets.value.push({
+    weight: null,
+    reps: null,
+    distance: null,
+    duration: null,
+  } as ExerciseSet)
+}
+
+function removeSet() {
+  if (exerciseSets.value.length) {
+    exerciseSets.value.pop()
+  }
+}
+
 /**
  * @todo
  * - Booleans control what shows up and if you can add sets
@@ -37,60 +52,65 @@ const text: Ref<number> = ref(0)
 </script>
 
 <template>
-  <QInput
-    v-if="trackDuration"
-    v-model.number="text"
-    label="Duration"
-    type="number"
-    dense
-    outlined
-    color="primary"
-    style="max-width: 120px"
-  >
-    <template v-slot:prepend>
-      <QIcon name="event" />
-    </template>
-  </QInput>
-  <QInput
-    v-if="trackDistance"
-    v-model.number="text"
-    label="Distance"
-    type="number"
-    dense
-    outlined
-    color="primary"
-    style="max-width: 120px"
-  >
-    <template v-slot:prepend>
-      <QIcon name="event" />
-    </template>
-  </QInput>
-  <QInput
-    v-if="trackWeight"
-    v-model.number="text"
-    label="Weight"
-    type="number"
-    dense
-    outlined
-    color="primary"
-    style="max-width: 120px"
-  >
-    <template v-slot:prepend>
-      <QIcon name="event" />
-    </template>
-  </QInput>
-  <QInput
-    v-if="trackReps"
-    v-model.number="text"
-    label="Reps"
-    type="number"
-    dense
-    outlined
-    color="primary"
-    style="max-width: 120px"
-  >
-    <template v-slot:prepend>
-      <QIcon name="event" />
-    </template>
-  </QInput>
+  <QBtn v-if="trackMultipleSets" label="Add Set" color="positive" @click="addSet()" />
+  <QBtn v-if="trackMultipleSets" label="Remove Set" color="negative" @click="removeSet()" />
+
+  <div v-for="(set, i) in props.exerciseSets" :key="i">
+    <QInput
+      v-if="trackDuration"
+      v-model.number="text"
+      label="Duration"
+      type="number"
+      dense
+      outlined
+      color="primary"
+      style="max-width: 120px"
+    >
+      <template v-slot:prepend>
+        <QIcon name="event" />
+      </template>
+    </QInput>
+    <QInput
+      v-if="trackDistance"
+      v-model.number="text"
+      label="Distance"
+      type="number"
+      dense
+      outlined
+      color="primary"
+      style="max-width: 120px"
+    >
+      <template v-slot:prepend>
+        <QIcon name="event" />
+      </template>
+    </QInput>
+    <QInput
+      v-if="trackWeight"
+      v-model.number="text"
+      label="Weight"
+      type="number"
+      dense
+      outlined
+      color="primary"
+      style="max-width: 120px"
+    >
+      <template v-slot:prepend>
+        <QIcon name="event" />
+      </template>
+    </QInput>
+    <QInput
+      v-if="trackReps"
+      v-model.number="text"
+      label="Reps"
+      type="number"
+      dense
+      outlined
+      color="primary"
+      style="max-width: 120px"
+    >
+      <template v-slot:prepend>
+        <QIcon name="event" />
+      </template>
+    </QInput>
+  </div>
 </template>
