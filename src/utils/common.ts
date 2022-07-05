23720bd91e4071409bbc2bd398c607af7168dede
michaeljoy255 <kodiakchaos@gmail.com>
2022-07-05 19:25:56 -0400

    truncate bug fix, measurements vis corrections

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -61,8 +61,12 @@ export function isDataInArray(arr: any[]): boolean {
  * @returns Truncated string with a '...' at the end
  */
 export function truncateString(str: string, len: number): string {
-  if (str.length <= len) {
-    return str
+  if (str) {
+    if (str.length <= len) {
+      return str
+    } else {
+      return str.slice(0, len) + '...'
+    }
   }
-  return str.slice(0, len) + '...'
+  return '-'
 }
